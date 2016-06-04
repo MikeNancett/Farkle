e6616dd3431ba8411b997af4813da0356358abfc
Mike Nancett <mike.nancett@gmail.com>
2016-06-04 10:05:29 -0500

    Initial Commit

---
+++ Farkle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Farkle
+//
+//  Created by Mike Nancett on 6/4/16.
+//  Copyright © 2016 Mike Nancett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
