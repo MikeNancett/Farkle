77d5a6321828edef7bdd818cead95042a2beba41
Mike Nancett <mike.nancett@gmail.com>
2016-06-05 17:19:34 -0500

    current state

---
+++ Farkle/DieLabel.h
@@ -0,0 +1,13 @@
+//
+//  DieLabel.h
+//  Farkle
+//
+//  Created by Mike Nancett on 6/4/16.
+//  Copyright © 2016 Mike Nancett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DieLabel : UILabel
+
+@end
---
+++ Farkle/DieLabel.m
@@ -0,0 +1,21 @@
+//
+//  DieLabel.m
+//  Farkle
+//
+//  Created by Mike Nancett on 6/4/16.
+//  Copyright © 2016 Mike Nancett. All rights reserved.
+//
+
+#import "DieLabel.h"
+
+@implementation DieLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
